8c7cb29b86e09ef96483acb4ae7a7f8a3c6bc460
KG_Vizek <karol.gaca.web@gmail.com>
2021-09-10 01:20:36 +0200

    admin register models, add test things in DB

--- rentproject/rentapp/admin.py
+++ rentproject/rentapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Brand, PowerTool
 
 admin.site.register(Profile)
+admin.site.register(Brand)
+admin.site.register(PowerTool)
--- rentproject/rentapp/models.py
+++ rentproject/rentapp/models.py
@@ -12,3 +12,94 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, blank=True, unique=True)
+    logo = models.ImageField(default='default.jpg', upload_to='brand_logo')
+    history = models.TextField
+
+    def __str__(self):
+        return f'{self.name} tools'
+
+
+class PowerTool(models.Model):
+    CORDLESSDRILL = 'CD'
+    HAMMERDRILL = 'HD'
+    IMPACTWRENCH = 'IW'
+    ROTARYHAMMER = 'RH'
+    IMPACTDRIVER = 'ID'
+    ELECTRICSCREWDRIVER = 'ES'
+    ROTARYTOOL = 'RT'
+    JIGSAW = 'JS'
+    RECIPROCATINGSAW = 'RS'
+    CIRCULARSAW = 'CS'
+    MITERSAW = 'MS'
+    BANDSAW = 'BS'
+    TABLESAW = 'TS'
+    CHAINSAW = 'HS'
+    BISCUITJOINER = 'BJ'
+    ANGLEGRINDER = 'AG'
+    BENCHGRINDER = 'BG'
+    SHOPVAC = 'SV'
+    BELTSANDER = 'LS'
+    RANDOMORBITALSANDER = 'RO'
+    DISCSANDER = 'DS'
+    WOODROUTER = 'WR'
+    NAILGUN = 'NG'
+    AIRCOMPRESSOR = 'AC'
+    MOISTUREMETER = 'MM'
+
+    TYPE_OF_TOOL = [
+        (CORDLESSDRILL, 'Cordless Drill'),
+        (IMPACTDRIVER, 'Impact Driver'),
+        (HAMMERDRILL, 'Hammer Drill'),
+        (ROTARYHAMMER, 'Rotary Hammer'),
+        (IMPACTWRENCH, 'Impact Wrench'),
+        (ELECTRICSCREWDRIVER, 'Electric Screwdriver'),
+        (ROTARYTOOL, 'Rotary Tool'),
+        (JIGSAW, 'Jigsaw'),
+        (RECIPROCATINGSAW, 'Reciprocating Saw'),
+        (CIRCULARSAW, 'Circular Saw'),
+        (MITERSAW, 'Miter Saw'),
+        (BANDSAW, 'Band Saw'),
+        (TABLESAW, 'Table Saw'),
+        (CHAINSAW, 'Chainsaw'),
+        (BISCUITJOINER, 'Biscuit Joiner'),
+        (ANGLEGRINDER, 'Angle Grinder'),
+        (BENCHGRINDER, 'Bench Grinder'),
+        (SHOPVAC, 'Shop Vac'),
+        (BELTSANDER, 'Belt Sander'),
+        (RANDOMORBITALSANDER, 'Random Orbital Sander'),
+        (DISCSANDER, 'Disc Sander'),
+        (WOODROUTER, 'Wood Router'),
+        (NAILGUN, 'Nail Gun'),
+        (AIRCOMPRESSOR, 'Air Compressor'),
+        (MOISTUREMETER, 'Moisture Meter'),
+    ]
+
+    BRANDNEW = 'BN'
+    IDEALSHAPE = 'IS'
+    GOOD = 'GC'
+    USED = 'US'
+    HEAVILYUSED = 'HU'
+
+    CONDITION = [
+        (BRANDNEW, 'Brand new'),
+        (IDEALSHAPE, 'Ideal shape'),
+        (GOOD, 'Good condition'),
+        (USED, 'Used'),
+        (HEAVILYUSED, 'heavily used')
+    ]
+    brand = models.ForeignKey(Brand, to_field='name', on_delete=models.CASCADE)
+    description = models.CharField(max_length=300, blank=True)
+    power = models.IntegerField(null=False)
+    type = models.CharField(max_length=2,
+                            choices=TYPE_OF_TOOL, )
+    condition = models.CharField(max_length=2,
+                                 choices=CONDITION, )
+    price = models.SmallIntegerField(null=False)
+    deposit = models.SmallIntegerField(null=False)
+
+    def __str__(self):
+        return f'{self.brand} {self.type} with power of {self.power} W'
