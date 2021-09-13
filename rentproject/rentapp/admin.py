4e9c8b2035516d1d2997a133b0955bc99252ba6c
KG_Vizek <karol.gaca.web@gmail.com>
2021-09-13 21:51:25 +0200

    no activation @ but working tool add

--- rentproject/rentapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Brand, PowerTool
-
-admin.site.register(Profile)
-admin.site.register(Brand)
-admin.site.register(PowerTool)
-
--- rentproject/rentapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountsConfig(AppConfig):
-    name = 'rentapp'
-
-    def ready(self):
-        import rentapp.signals
